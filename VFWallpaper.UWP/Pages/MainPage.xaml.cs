02caa076b3d612d1c93df1e8ac093fdd77d6937b
Andreas Furster <andreas.furster@hotmail.com>
2018-09-27 23:56:08 +0200

    Working prototype!

--- VFWallpaper.UWP/Pages/MainPage.xaml.cs
+++ VFWallpaper.UWP/Pages/MainPage.xaml.cs
@@ -1,21 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.System.UserProfile;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Media.Imaging;
+using VFWallpaper.UWP.Utilities;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace VFWallpaper.UWP
+namespace VFWallpaper.UWP.Pages
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -25,6 +19,52 @@ namespace VFWallpaper.UWP
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+            var pickedFile = await PickFile();
+            
+            Log($"File picked: {pickedFile.Name}");
+
+            // TODO: Make configurable 
+            const double speed = 4;
+            const int delay = 1;
+            var offset = TimeSpan.FromMinutes(2);
+
+            var resolution = ApplicationView.GetForCurrentView().VisibleBounds;
+            var fe = new FrameExtractor(pickedFile, resolution);
+
+            // TODO: Do this stuff in a task
+            for (int i = 10; i < 100; i++)
+            {
+                Log($"Start extracting frame: {i}");
+                var file = await fe.ExtractFrame(TimeSpan.FromSeconds(i * speed) + offset);
+
+                Log($"Set as background");
+                await UserProfilePersonalizationSettings.Current.TrySetWallpaperImageAsync(file);
+
+                Log($"Wait {delay} seconds");
+                await Task.Delay(TimeSpan.FromSeconds(delay));
+            }
+
+        }
+
+        private async Task<StorageFile> PickFile()
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.VideosLibrary;
+
+            picker.FileTypeFilter.Add(".mp4");
+            picker.FileTypeFilter.Add(".mkv");
+
+            return await picker.PickSingleFileAsync();
+        }
+
+        private void Log(string text)
+        {
+            LogTextBlock.Text += text + Environment.NewLine;
         }
     }
 }
--- VFWallpaper.UWP/Utilities/FrameExtractor.cs
+++ VFWallpaper.UWP/Utilities/FrameExtractor.cs
@@ -1,12 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Graphics.Imaging;
+using Windows.Media.Editing;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace VFWallpaper.UWP.Utilities
 {
     class FrameExtractor
     {
+        private Rect resolution;
+        private StorageFile file;
+
+        public FrameExtractor(StorageFile file, Rect resolution)
+        {
+            this.file = file;
+            this.resolution = resolution;
+        }
+
+        public async Task<StorageFile> ExtractFrame(TimeSpan timeOfFrame)
+        {
+
+            if (file == null)
+            {
+                throw new ArgumentNullException(nameof(file));
+            }
+
+            var imageStream = await GetImageStream(timeOfFrame, file);
+            return await SaveFrame(imageStream);
+        }
+
+        private async Task<ImageStream> GetImageStream(TimeSpan timeOfFrame, StorageFile pickedFile)
+        {
+            var clip = await MediaClip.CreateFromFileAsync(pickedFile);
+            var composition = new MediaComposition();
+            composition.Clips.Add(clip);
+
+            var imageStream = await composition.GetThumbnailAsync(timeOfFrame, (int) resolution.Width, (int) resolution.Height,
+                VideoFramePrecision.NearestFrame);
+            return imageStream;
+        }
+
+        private async Task<StorageFile> SaveFrame(ImageStream imageStream)
+        {
+            //generate bitmap 
+            var writableBitmap = new WriteableBitmap((int)resolution.Width, (int)resolution.Height);
+            writableBitmap.SetSource(imageStream);
+
+            //generate some random name for file in PicturesLibrary
+            var file = await ApplicationData.Current.LocalFolder.CreateFileAsync("CurrentWallpaper_" + Guid.NewGuid().ToString().Substring(0, 4) + ".jpg");
+            
+            //get stream from bitmap
+            Stream stream = writableBitmap.PixelBuffer.AsStream();
+            byte[] pixels = new byte[(uint)stream.Length];
+            await stream.ReadAsync(pixels, 0, pixels.Length);
+
+            using (var writeStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, writeStream);
+                encoder.SetPixelData(
+                    BitmapPixelFormat.Bgra8,
+                    BitmapAlphaMode.Premultiplied,
+                    (uint)writableBitmap.PixelWidth,
+                    (uint)writableBitmap.PixelHeight,
+                    96,
+                    96,
+                    pixels);
+                await encoder.FlushAsync();
+
+                using (var outputStream = writeStream.GetOutputStreamAt(0))
+                {
+                    await outputStream.FlushAsync();
+                }
+            }
+
+            return file;
+        }
     }
 }
